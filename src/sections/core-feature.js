75661ed5052cb32396e77fd1c800524138487794
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2022-03-01 19:20:14 +0530

    footer

--- src/sections/core-feature.js
+++ src/sections/core-feature.js
@@ -1,5 +1,5 @@
 /** @jsx jsx */
-import { jsx, Container, Box, Image } from 'theme-ui';
+import { jsx, Container, Box, Image, } from 'theme-ui';
 import TextFeature from 'components/text-feature';
 
 import FeatureThumb from 'assets/core-feature.png';
