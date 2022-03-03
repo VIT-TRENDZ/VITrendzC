ae2e51926950516dd08c5f501d4e2981d0f9d9c6
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2022-03-03 08:08:57 +0530

    Out

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
-
-module.exports = withPlugins([optimizedImages], { target: 'serverless' });
+module.exports = withPlugins([optimizedImages], { target: 'serverless',webpack5: true, experimental: { modern: true, esmExternals: true, }, reactStrictMode: true, images: { domains: [],disableStaticImages: true },});
--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,3 +1,4 @@
+/** @jsxRuntime classic */
 /** @jsx jsx */
 import { jsx, Box, Container, Image, Text, Heading,Grid} from 'theme-ui';
 import { Link } from 'components/link';
